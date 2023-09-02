ea624e3b5f75bb526e59b441631dd99395ed70f9
Eugene Dudkin <ZheDre1N@gmail.com>
2023-09-02 15:15:27 +0300

    Initial project of lecture 1

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Eugene Dudkin on 02.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
