1e53f1daddc15ec740bcfa0ee4c6c8917396532f
Eugene Dudkin <ZheDre1N@gmail.com>
2023-09-05 00:15:44 +0300

    HW lecture 2

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,10 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State var emojis = ["🤯", "😎", "🍔", "🎲"]
+  @State var currentEmojisSet = EmojiesSet.empty
 
   var body: some View {
     VStack {
+      Text("Memorize!")
+        .font(.largeTitle)
       ScrollView {
         cards
       }
@@ -21,41 +23,104 @@ struct ContentView: View {
   }
   
   var cards: some View {
-    LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))])  {
-      ForEach(emojis.indices, id: \.self) { index in
-        CardView(isFacedUp: true, content: emojis[index])
-          .aspectRatio(2/3, contentMode: .fit)
+    LazyVGrid(columns: gridItems(for: currentEmojisSet.emojis.count))  {
+      ForEach(currentEmojisSet.emojis.indices, id: \.self) { index in
+        CardView(isFacedUp: false, content: currentEmojisSet.emojis[index])
+          .aspectRatio(0.5, contentMode: .fit)
       }
     }
-    .foregroundStyle(.blue)
+    .foregroundStyle(currentEmojisSet.color)
   }
   
-  @ViewBuilder
   var buttons: some View {
-      Button("Add card") {
-        emojis.append(emojis.randomElement()!)
+    HStack(alignment: .lastTextBaseline) {
+      Spacer()
+      ThemeButton(text: "Vehicles", imageSystemName: "car") {
+        currentEmojisSet = .vehiclesEmojis
       }
-      .foregroundStyle(.blue)
-      Button("Reset") {
-        emojis = ["🤯", "😎", "🍔", "🎲"]
+      Spacer()
+      ThemeButton(text: "Sports", imageSystemName: "figure.run") {
+        currentEmojisSet = .sportsEmojis
       }
+      Spacer()
+      ThemeButton(text: "Countries", imageSystemName: "flag") {
+        currentEmojisSet = .countriesEmojis
+      }
+      Spacer()
+    }
+    .foregroundStyle(.primary)
+  }
+  
+  private func gridItems(for count: Int) -> [GridItem] {
+    switch count {
+    case 0..<9:
+      return [GridItem(), GridItem(), GridItem()]
+    case 9..<12:
+      return [GridItem(), GridItem(), GridItem(), GridItem()]
+    default:
+      return [GridItem(), GridItem(), GridItem(), GridItem(), GridItem()]
+    }
+    
+  }
+}
+
+struct EmojiesSet {
+  let color: Color
+  let emojis: Array<Character>
+  
+  static let empty = Self(color: .black, emojis: Array<Character>())
+  
+  static let vehiclesEmojis = Self(
+    color: .red,
+    emojis: ["✈️", "🚗", "🚀", "🚘", "🚙", "🚎", "🚛", "🚕", "🚁", "🏎️", "🚒"]
+  )
+  
+  static let sportsEmojis = Self(
+    color: .orange,
+    emojis: ["⚽️", "🏀", "🏈", "⚾️", "🏓", "🏒", "🚴‍♀️", "🥊", "🥋", "🏄‍♂️"]
+  )
+  
+  static let countriesEmojis = Self(
+    color: .blue,
+    emojis: ["🇬🇧", "🇺🇸", "🇫🇲", "🇩🇰", "🇦🇪", "🇵🇭", "🇵🇰", "🇷🇸", "🇫🇮", "🇾🇪"]
+  )
+}
+
+struct ThemeButton: View {
+  let text: String
+  let imageSystemName: String
+  let action: () -> Void
+  
+  var body: some View {
+    Button(action: {
+      action()
+    }, label: {
+      VStack(content: {
+        Image(systemName: imageSystemName)
+          .font(.title)
+        Text(text)
+          .font(.subheadline)
+      })
+    })
   }
 }
 
 struct CardView: View {
   @State var isFacedUp: Bool = false
-  let content: String
+  let content: Character
   
   var body: some View {
     ZStack {
       let base = RoundedRectangle(cornerRadius: 10)
-
-      Group {
-        base.fill(.white)
-        base.strokeBorder(style: .init(lineWidth: 2))
-        Text(content).font(.largeTitle)
-      }
-      base.fill().opacity(isFacedUp ? 0 : 1)
+      base
+        .fill(.white)
+      base
+        .strokeBorder(style: .init(lineWidth: 2))
+      Text(String(content))
+        .font(.largeTitle)
+      base
+        .fill()
+        .opacity(isFacedUp ? 0 : 1)
     }
     .onTapGesture {
       isFacedUp.toggle()
